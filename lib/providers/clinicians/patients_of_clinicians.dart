6b73d6a8bd63cd97efa62a610d92ebb346806483
anastasiamela <anastasia.mela11@gmail.com>
2020-09-18 16:41:58 +0300

    Added the patients of clinicians Model

---
+++ lib/providers/clinicians/patients_of_clinicians.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../../models/http_exception.dart';
+
+class PatientOfClinician with ChangeNotifier {
+  final String patientId;
+  PatientOfClinician({this.patientId});
+}
+
+class PatientsOfClinician with ChangeNotifier {
+  List<PatientOfClinician> _patients = [];
+
+  PatientsOfClinician();
+
+  List<PatientOfClinician> get patients {
+    return [..._patients];
+  }
+
+  Future<void> fetchAndSetPatients(String clinicianId) async {
+    try {
+      final response = await FirebaseFirestore.instance
+          .collection('clinicians')
+          .doc(clinicianId)
+          .collection('patients')
+          .orderBy("createdAt", descending: true)
+          .get();
+      final extractedData = response.docs;
+
+      if (extractedData == null) {
+        return;
+      }
+      final List<PatientOfClinician> loadedPatients = [];
+      extractedData.forEach((patient) {
+        var patientData = patient.data();
+        loadedPatients.add(
+          PatientOfClinician(
+            patientId: patientData['patientId'],
+          ),
+        );
+      });
+      _patients = loadedPatients;
+      notifyListeners();
+    } catch (error) {
+      throw (error);
+    }
+  }
+
+  Future<void> addPatient(
+      PatientOfClinician patientInput, String clinicianId) async {
+    final timestamp = DateTime.now();
+    try {
+      await FirebaseFirestore.instance
+          .collection('clinicians')
+          .doc(clinicianId)
+          .collection('patients')
+          .doc(patientInput.patientId)
+          .set({
+        'patientId': patientInput.patientId,
+        'createdAt': Timestamp.fromDate(timestamp),
+      });
+      final newPatient = PatientOfClinician(
+        patientId: patientInput.patientId,
+      );
+      _patients.add(newPatient);
+      notifyListeners();
+    } catch (error) {
+      print(error);
+      throw error;
+    }
+  }
+
+  Future<void> deletePatient(String id, String clinicianId) async {
+    final existingPatientIndex =
+        _patients.indexWhere((patient) => patient.patientId == id);
+    var existingPatient = _patients[existingPatientIndex];
+    _patients.removeAt(existingPatientIndex);
+    notifyListeners();
+    try {
+      await FirebaseFirestore.instance
+          .collection('clinicians')
+          .doc(clinicianId)
+          .collection('patients')
+          .doc(id)
+          .delete();
+      existingPatient = null;
+    } catch (error) {
+      patients.insert(existingPatientIndex, existingPatient);
+      notifyListeners();
+      throw HttpException('Could not delete the patient.');
+    }
+  }
+}
